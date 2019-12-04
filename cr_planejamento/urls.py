bde3bc5be77ffe7ae5a0a577d20ef56709f75d9b
João Miranda <jvdmmiranda@gmail.com>
2019-12-04 17:58:41 -0300

    Start using drf

--- cr_planejamento/urls.py
+++ cr_planejamento/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
+from student.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls')),
+    path('appraisal/', include('student.urls', namespace='appraisal')),
 ]
