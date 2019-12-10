8924c97f3028f32ea595adc46d0cd022b6910167
João Miranda <jvdmmiranda@gmail.com>
2019-12-10 18:11:58 -0300

    Removes all

--- backend/student/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from appraisal.serializers import *
-from .models import *
-
-class StudentSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Student
-        fields = ('id', 'name')
