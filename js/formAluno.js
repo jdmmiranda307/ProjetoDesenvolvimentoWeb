3264d45bf34b3d4146a5bef8f714f570effedbfe
João Miranda <jvdmmiranda@gmail.com>
2019-12-11 15:43:47 -0300

    Adds CRUD for aluno

---
+++ js/formAluno.js
@@ -0,0 +1,6 @@
+const formAlunoContent = "<form>"+
+                           "<div class='form-group'>"+
+                              "<label for='name-field'>Nome</label>"+
+                              "<input type='text' class='form-control' id='name-field' placeholder=name'>"+
+                           "</div>"+
+                        "</form>"
