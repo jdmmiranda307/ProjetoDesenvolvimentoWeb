5d7ad79cdb0044cc91019173aa0e1fdabde60f8c
João Miranda <jvdmmiranda@gmail.com>
2019-12-11 15:53:54 -0300

    Adds pages

---
+++ docs/js/formAppraisal.js
@@ -0,0 +1,106 @@
+const formAppraisalContent =             "<form>"+
+               "<div class='form-group'>"+
+                  "<label for='aluno-field'>Aluno</label>"+
+                  "<select class='form-control' id='aluno-select'>"+
+                     "<option>1</option>"+
+                     "<option>2</option>"+
+                     "<option>3</option>"+
+                     "<option>4</option>"+
+                     "<option>5</option>"+
+                  "</select>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='peso-field'>Peso</label>"+
+                  "<input type='text' class='form-control' id='peso-field' placeholder='00'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='altura-field'>Altura (cm)</label>"+
+                  "<input type='text' class='form-control' id='altura-field' placeholder='000'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='percent-field'>Percentual de gordura</label>"+
+                  "<input type='text' class='form-control' id='percent-field' placeholder='percent'>"+
+               "</div>"+
+               "<h2>Pregas Cutaneas (mm)</h2>"+
+               "<div class='form-group'>"+
+                  "<label for='biceps-field'>biceps</label>"+
+                  "<input type='text' class='form-control' id='biceps-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='triceps-field'>Triceps</label>"+
+                  "<input type='text' class='form-control' id='triceps-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='abs-field'>Abdomen</label>"+
+                  "<input type='text' class='form-control' id='abs-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='axilar-field'>Axilar</label>"+
+                  "<input type='text' class='form-control' id='axilar-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='suprailiac-field'>Supra Iliaca</label>"+
+                  "<input type='text' class='form-control' id='suprailiac-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='subscapular-field'>Subscapular</label>"+
+                  "<input type='text' class='form-control' id='subscapular-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='peito-field'>Peito</label>"+
+                  "<input type='text' class='form-control' id='peito-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='coxa-field'>Coxa</label>"+
+                  "<input type='text' class='form-control' id='coxa-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='calf-field'>Panturrilha Medial</label>"+
+                  "<input type='text' class='form-control' id='calf-field'>"+
+               "</div>"+
+               "<h2>Medidas (cm)</h2>"+
+               "<div class='form-group'>"+
+                  "<label for='peitomeasure-field'>Peito</label>"+
+                  "<input type='text' class='form-control' id='peitomeasure-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='cintura-field'>Cintura</label>"+
+                  "<input type='text' class='form-control' id='cintura-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='abdomenmeasure-field'>Abdomen</label>"+
+                  "<input type='text' class='form-control' id='abdomenmeasure-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='quadril-field'>Quadril</label>"+
+                  "<input type='text' class='form-control' id='quadril-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='coxameasure-field'>Coxa</label>"+
+                  "<input type='text' class='form-control' id='coxameasure-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='coxa-pr-field'>Coxa Proximal</label>"+
+                  "<input type='text' class='form-control' id='coxa-pr-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='pantu-field'>Panturrilha</label>"+
+                  "<input type='text' class='form-control' id='pantu-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='relaxed-arm-field'>Braço relaxado</label>"+
+                  "<input type='text' class='form-control' id='relaxed-arm-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='contracted-arm-field'>Braço Contraido</label>"+
+                  "<input type='text' class='form-control' id='contracted-arm-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='forarm-field'>Antebraço</label>"+
+                  "<input type='text' class='form-control' id='forarm-field'>"+
+               "</div>"+
+               "<div class='form-group'>"+
+                  "<label for='punho-field'>Punho</label>"+
+                  "<input type='text' class='form-control' id='punho-field'>"+
+               "</div>"+
+            "</form>"
